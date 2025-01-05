603874be9e1b4aa69b682c8e11332f315ef41a41
Maxim Gordeev <m.tehnologiy@gmail.com>
2025-01-06 01:00:18 +0300

    main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ def chat():
 
 # The block below is for local development only. In production, Gunicorn will run the app.
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get('PORT', 10000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
